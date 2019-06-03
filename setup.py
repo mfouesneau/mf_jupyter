615e9472dd7a4b48ba463729496bb2b61f2a36a2
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2019-06-03 16:34:51 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = "mf_jupyter",
+    version = 1.0,
+    description = "Collection of tools to export ipynb to latex documents",
+    long_description = readme(),
+    author = "Morgan Fouesneau",
+    author_email = "",
+    url = "https://github.com/mfouesneau/mf_jupyter",
+    packages = find_packages(),
+    package_data = {'mf_jupyter':['./*'],},
+    include_package_data = True,
+    classifiers=[
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Astronomy'
+      ],
+    zip_safe=False
+)
+
