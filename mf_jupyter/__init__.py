c4d95d57657e8d23ff4d9a400d7365d7b8e8ca28
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2021-02-16 19:43:34 +0100

    Update __init__.py
    Added function to compile from notebook

--- mf_jupyter/__init__.py
+++ mf_jupyter/__init__.py
@@ -11,3 +11,70 @@ def get_package_path():
 
 
 
+def mf_jupyter_convert(filename: str, template: str = "dpac", silent: bool = True, texdirs: list = []):
+    """ This function compiles a notebook into a DPAC latex document and its PDF.
+    
+    Parameters
+    ----------
+    filename: str
+        name of the notebook without extension
+       
+    template: str
+        mf_jupyter template to use
+    
+    silent: bool
+        do not show the stdout/stderr of the compilation if set.
+        
+    texdirs: list[str]
+        optional directories to include for latex
+    """
+
+    import subprocess
+    import mf_jupyter
+    from subprocess import Popen, PIPE, STDOUT
+
+    MF_JUPYTER_DIR = mf_jupyter.get_package_path()
+    CONFIG_FILE = f"{MF_JUPYTER_DIR:s}/config.py"
+    LATEXOPTS = f"--to latex --config {CONFIG_FILE:s}"
+    TEMPLATEOPTS = f"--template {MF_JUPYTER_DIR:s}/templates"
+    PDFLATEX = f"pdflatex -enable-write18 -shell-escape -interaction=nonstopmode"
+    if template == 'dpac':
+        TEMPLATE = f"mf_dpac.tplx"
+    else:
+        TEMPLATE = template
+
+
+    cmd = f"jupyter nbconvert {LATEXOPTS:s} {filename:s}.ipynb {TEMPLATEOPTS:s}/{TEMPLATE:s}"
+    print(cmd)
+    
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    for line in p.stdout:
+        if not silent:
+            print(line.decode().rstrip())
+        else:
+            print('.', end='')
+    print("\nconversion to tex: ", "success" if int(p.poll()) == 0 else "fail")
+
+    TEXEXPORTS = [f"{MF_JUPYTER_DIR:s}"]
+    if texdirs:
+        for k in texdirs:
+            if k[-2] != '//':
+                TEXEXPORTS.append(k + '//')
+            else:
+                TEXEXPORT.append(k)
+    
+    TEXEXPORT = ':'.join(TEXEXPORT)    
+        
+    cmd = f"""export TEXINPUTS=.:{TEXEXPORT:s}:
+    {PDFLATEX:s} {filename:s}
+    bibtex {filename:s}
+    {PDFLATEX:s} {filename:s}
+    {PDFLATEX:s} {filename:s}"""
+    print(cmd)
+    p = Popen(cmd.replace('\n', '; '), shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    for line in p.stdout:
+        if not silent:
+            print(line.decode().rstrip())
+        else:
+            print('.', end='')
+    print(f"\nconversion finished. Check {filename:s}.pdf")
