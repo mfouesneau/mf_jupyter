d80b6b4a22fcb1ffcdcb7e67c24a33dad070a734
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2023-04-12 17:34:00 +0200

    cmds

--- src/mf_jupyter/__init__.py
+++ src/mf_jupyter/__init__.py
@@ -1,7 +1,9 @@
 from importlib.metadata import version, PackageNotFoundError
+import pkg_resources
 
 try:
     __version__ = version("mf_jupyter")
+    templates_dir = pkg_resources.resource_filename('mf_jupyter', 'templates')
 except PackageNotFoundError:
     # package is not installed
     pass
---
+++ src/mf_jupyter/convert_cmd.py
@@ -0,0 +1,134 @@
+""" Generate commandlines to run the conversions """
+import os
+import pkg_resources
+from typing import List
+
+pkg_dir = pkg_resources.resource_filename('mf_jupyter', '')
+
+def get_nbconvert_cmd(notebook_path: str,
+                      template: str = 'tufte',
+                      debug: bool = True) -> str:
+    """ Get commandline to run nbconvert
+
+    Parameters
+    ----------
+    notebook_path: str
+        path to notebook to convert
+    template: str
+        name of the template to use
+    debug: bool
+        set to True to enable debug mode of nbconvert
+
+    Returns
+    -------
+    cmd: str
+        the commandline to run nbconvert
+    """
+    config = os.path.abspath(pkg_resources.resource_filename('mf_jupyter', 'config.py'))
+    templates_dir = os.path.abspath(pkg_resources.resource_filename('mf_jupyter', 'templates'))
+
+    args = [
+        """jupyter nbconvert """,
+        f"""--config={config:s}""",
+        """--to=latex""",
+        f"""--template={template:s}""",
+        f"""--TemplateExporter.extra_template_basedirs={templates_dir:s}""",
+        f"""{notebook_path:s}"""]
+
+    if debug:
+        args.append('--debug')
+
+    return ' '.join(args)
+
+def get_pdflatex_cmd(latexfile_path: str,
+                     texdirs: List[str] = [],
+                     ) -> str:
+    """Get commandline to run pdflatex
+
+    Parameters
+    ----------
+    latexfile_path: str
+        path to latex file to convert
+    texdirs: list[str]
+        optional directories to include for latex
+
+    Returns
+    -------
+    cmd: str
+        the commandline to run pdflatex
+    """
+    TEXMFLOCAL = os.path.join(
+                        os.path.abspath(pkg_resources.resource_filename('mf_jupyter', 'texlibs')),
+                        'texmf//')
+    PDFLATEX = f"pdflatex -enable-write18 -shell-escape -interaction=nonstopmode"
+
+    TEXEXPORTS = [f"{TEXMFLOCAL:s}"]
+    if texdirs:
+        for k in texdirs:
+            if k[-2] != '//': #add anything recurrently
+                TEXEXPORTS.append(k + '//')
+            else:
+                TEXEXPORTS.append(k)
+
+    TEXEXPORTS = ':'.join(TEXEXPORTS)
+
+    cmd = f"""export TEXINPUTS=.:{TEXEXPORTS:s}:
+    {PDFLATEX:s} {latexfile_path:s}
+    bibtex {latexfile_path:s}
+    {PDFLATEX:s} {latexfile_path:s}
+    {PDFLATEX:s} {latexfile_path:s}"""
+
+    return cmd
+
+
+def mf_jupyter_convert(filename: str,
+                       template: str = "tufte",
+                       silent: bool = True,
+                       texdirs: list = []):
+    """ This function compiles a notebook into a DPAC latex document and its PDF.
+
+    Parameters
+    ----------
+    filename: str
+        name of the notebook without extension
+
+    template: str
+        mf_jupyter template to use
+
+    silent: bool
+        do not show the stdout/stderr of the compilation if set.
+
+    texdirs: list[str]
+        optional directories to include for latex
+    """
+    from subprocess import Popen, PIPE, STDOUT
+
+    # Make sure we have a filename without extension
+    filename = filename.replace('.ipynb', '')
+
+    # step 1: convert notebook to latex
+    cmd = get_nbconvert_cmd(filename, template=template, debug=~silent)
+    print(cmd)
+
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    for line in p.stdout:
+        if not silent:
+            print(line.decode().rstrip())
+        else:
+            print('.', end='')
+    print("\nconversion to tex: ", "success" if int(p.poll()) == 0 else "fail")
+    if p.poll()!= 0:
+        raise RuntimeError("nbconvert step failed")
+
+    # step 2: convert latex to pdf
+    cmd = get_pdflatex_cmd(filename, texdirs=texdirs)
+    print(cmd)
+
+    p = Popen(cmd.replace('\n', '; '), shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    for line in p.stdout:
+        if not silent:
+            print(line.decode().rstrip())
+        else:
+            print('.', end='')
+
+    print(f"\nconversion finished. Check {filename:s}.pdf")
