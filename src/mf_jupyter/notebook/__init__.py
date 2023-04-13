2f72af0037a2bf95d142a521795277f9c4f79039
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2023-04-13 16:13:35 +0200

    imports

--- src/mf_jupyter/notebook/__init__.py
+++ src/mf_jupyter/notebook/__init__.py
@@ -1,3 +1,5 @@
 """ Tools for notebook writing """
-from . import jupyter
+from .jupyter import (register_magic, display_toolbar,
+                      add_input_toggle, add_hide_button,
+                      add_citation_button)
 from . import tools
